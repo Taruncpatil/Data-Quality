321979f3a5dae064210ca44b59f3685e90ed9219
Tarun <ptarun698@gmail.com>
2026-01-20 11:17:15 +0530

    2nd commit

---
+++ Stimulator/config.py
@@ -0,0 +1,5 @@
+DEVICE_IDS = ["device_1", "device_2", "device_3"]
+
+API_ENDPOINT = "http://localhost:5000/ingest"
+
+SEND_INTERVAL_SECONDS = 2
--- Stimulator/device_stimulation.py
+++ Stimulator/device_stimulation.py
@@ -3,12 +3,7 @@ import json
 import time
 import random
 import requests
-
-DEVICE_IDS = ["device_1", "device_2", "device_3"]
-
-API_ENDPOINT = "http://localhost:5000/ingest"
-
-SEND_INTERVAL_SECONDS = 2
+from config import DEVICE_IDS, API_ENDPOINT, SEND_INTERVAL_SECONDS
 
 def generate_data(device_id):
     data = {
